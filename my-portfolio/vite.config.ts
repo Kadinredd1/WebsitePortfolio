033a826886e5c46276cd5afaac4291fa3f0a6a17
kredd <kredd@vailsys.com>
2025-07-11 14:01:14 -0500

    deisgn changes

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults } from 'vitest/config'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   
   test: {
     environment: 'jsdom',
